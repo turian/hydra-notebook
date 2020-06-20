a91b287eadf57afa287742032897d4e14dc034e0
Joseph Turian <turian@gmail.com>
2020-06-21 01:31:04 +0200

    Code updates

---
+++ main.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import hydra.experimental
+hydra.experimental.initialize(config_path="conf")
+cfg=hydra.experimental.compose(config_name="config.yaml")
+
+hydra.experimental.initialize_with_module(calling_module="module", config_path="conf")
+# -
+
+cfg=hydra.experimental.compose(config_name="config.yaml")
+
+import module
+
+
